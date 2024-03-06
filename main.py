01a4c7618e6eccbaa82c14ecdfeb7f4243bf4e1c
Magomed Bekaev <bekaev.mag@gmail.com>
2024-03-06 10:21:41 +0300

    Changed based on suggestions from teacher

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import os
 
-with open("directory.txt", "r") as f:
-    ROOT_DIRECTORY = f.read().strip()
-    if ROOT_DIRECTORY == "":
-        ROOT_DIRECTORY = os.getcwd()
-    WORKING_DIRECTORY = ROOT_DIRECTORY
+global ROOT_DIRECTORY, WORKING_DIRECTORY
 
 
 def create_folder(folder_name):
@@ -38,12 +34,6 @@ def move_to_folder(folder_name):
             print(f"Спустились в папку '{folder_name}'.")
         else:
             print(f"Папка '{folder_name}' не найдена.")
-
-
-def list_contents():
-    print("Содержимое папки:")
-    for item in os.listdir(WORKING_DIRECTORY):
-        print(item)
 
 
 def create_file(file_name):
@@ -106,61 +96,67 @@ def rename_file(old_name, new_name):
     except FileNotFoundError:
         print(f"Файл '{old_name}' не найден.")
 
+def main():
+    with open("directory.txt", "r") as f:
+        ROOT_DIRECTORY = f.read().strip()
+        if ROOT_DIRECTORY == "":
+            ROOT_DIRECTORY = os.getcwd()
+        WORKING_DIRECTORY = ROOT_DIRECTORY
 
-while True:
-    print("\nТекущая рабочая директория:", WORKING_DIRECTORY)
-    print("1. Создать папку")
-    print("2. Удалить папку")
-    print("3. Перемещение по папкам")
-    print("4. Создать файл")
-    print("5. Записать в файл")
-    print("6. Показать содержимое файл")
-    print("7. Удаление файла")
-    print("8. Копирование файла")
-    print("9. Переместить файл")
-    print("10. Переименовать файл")
-    print("11. Просмотр содержимого папки")
-    print("12. Выход")
+    while True:
+        print("\nТекущая рабочая директория:", WORKING_DIRECTORY)
+        print("1. Создать папку")
+        print("2. Удалить папку")
+        print("3. Перемещение по папкам")
+        print("4. Создать файл")
+        print("5. Записать в файл")
+        print("6. Показать содержимое файл")
+        print("7. Удаление файла")
+        print("8. Копирование файла")
+        print("9. Переместить файл")
+        print("10. Переименовать файл")
+        print("11. Выход")
 
-    choice = input("Введите число для выбора: ")
+        choice = input("Введите число для выбора: ")
 
-    if choice == "1":
-        inp = input("Введите название папки: ")
-        create_folder(inp)
-    elif choice == "2":
-        inp = input("Введите название папки для удаления: ")
-        delete_folder(inp)
-    elif choice == "3":
-        inp = input("Введите название папки: ")
-        move_to_folder(inp)
-    elif choice == "4":
-        inp = input("Введите название файла: ")
-        create_file(inp)
-    elif choice == "5":
-        inp = input("Введите название файла: ")
-        write_to_file(inp)
-    elif choice == "6":
-        inp = input("Введите название файла: ")
-        view_file_contents(inp)
-    elif choice == "7":
-        inp = input("Введите название файла: ")
-        delete_file(inp)
-    elif choice == "8":
-        source_file = input("Введите название файла: ")
-        destination = input("Введите путь для копирования: ")
-        copy_file(source_file, destination)
-    elif choice == "9":
-        source_file = input("Введите название файла: ")
-        destination = input("Введите путь для перемещения: ")
-        move_file(source_file, destination)
-    elif choice == "10":
-        old_name = input("Введите название файла: ")
-        new_name = input("Введите новое название файла: ")
-        rename_file(old_name, new_name)
-    elif choice == "11":
-        list_contents()
-    elif choice == "12":
-        print("Выход")
-        break
-    else:
-        print("Нет такого варианта")
+        if choice == "1":
+            inp = input("Введите название папки: ")
+            create_folder(inp)
+        elif choice == "2":
+            inp = input("Введите название папки для удаления: ")
+            delete_folder(inp)
+        elif choice == "3":
+            inp = input("Введите название папки или '..': ")
+            move_to_folder(inp)
+        elif choice == "4":
+            inp = input("Введите название файла: ")
+            create_file(inp)
+        elif choice == "5":
+            inp = input("Введите название файла: ")
+            write_to_file(inp)
+        elif choice == "6":
+            inp = input("Введите название файла: ")
+            view_file_contents(inp)
+        elif choice == "7":
+            inp = input("Введите название файла: ")
+            delete_file(inp)
+        elif choice == "8":
+            source_file = input("Введите название файла: ")
+            destination = input("Введите путь для копирования: ")
+            copy_file(source_file, destination)
+        elif choice == "9":
+            source_file = input("Введите название файла: ")
+            destination = input("Введите путь для перемещения: ")
+            move_file(source_file, destination)
+        elif choice == "10":
+            old_name = input("Введите название файла: ")
+            new_name = input("Введите новое название файла: ")
+            rename_file(old_name, new_name)
+        elif choice == "11":
+            print("Выход")
+            break
+        else:
+            print("Нет такого варианта")
+
+if __name__ == '__main__':
+    main()
